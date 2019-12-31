ba8e518873c1de6866507476c1a7b9cb11b21230
Feras Deiratany <fdeiratany@level20.com>
2019-12-30 23:21:08 -0500

    add GA

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import ReactGA from 'react-ga';
+
+
 import Travel from './travel'
 import './App.css';
 
+function initializeReactGA() {
+  ReactGA.initialize('UA-65379042-2');
+  ReactGA.pageview('/homepage');
+}
+
 function App() {
+  initializeReactGA()
   return (
     <>
-    <Travel />
+      <Travel />
     </>
   );
 }
